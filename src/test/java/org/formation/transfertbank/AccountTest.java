99ec6f86fa7a3ef4704d78e63b7275a807fde150
Kevin Vaysse <vayssek@gmail.com>
2016-06-03 09:30:23 +0200

    ADD AccountTest.checkValidAccountValidation
    Check the conditional branch of the Account.isValid() when the result is true. Status : success

--- src/test/java/org/formation/transfertbank/AccountTest.java
+++ src/test/java/org/formation/transfertbank/AccountTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class AccountTest {
 	private static final Account FIRST_ACCOUNT = new Account("123_123456789",300);
 	private static final Account SECOND_ACCOUNT = new Account("123_123456788",0);
-	private static final Account FALSE_ACCOUNT = new Account("1232123456788",0);
+	private static final Account FALSE_ACCOUNT = new Account("123212345678",0);
 	private Account firstAccount;
 	private Account secondAccount;
 	private Account falseAccount;
@@ -42,12 +42,21 @@ public class AccountTest {
 	}
 
 	@Test
-	public void checkAccountValidation(){
+	public void checkFalseAccountValidation(){
 		//GIVEN
 		//WHEN
 		boolean result= falseAccount.isValid();
 		//THEN
 		assertEquals(false,result);
+	}
+	
+	@Test
+	public void checkTrueAccountValidation(){
+		//GIVEN
+		//WHEN
+		boolean result= firstAccount.isValid();
+		//THEN
+		assertEquals(true,result);
 	}
 	
 }
